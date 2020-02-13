691b7f7e48a7e47bb7d820d5f356cf240788e6dd
Eryk Kowalewicz <eryk.kowalewicz@outlook.com>
2020-02-13 13:34:04 +0000

    Added Car and Main

---
+++ Car.java
@@ -0,0 +1,18 @@
+class Car {
+
+    // instance variables
+    int topSpeed;
+    int year;
+    String manufacturer;
+    String model;
+    String colour;
+    float cost;
+    int seats;
+
+    int reduceSeats(){
+        
+        return seats--;
+
+    }
+
+}
